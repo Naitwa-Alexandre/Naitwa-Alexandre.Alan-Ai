2654d7efa6882c89c4a6e2e89112d2e288806210
Alexandre Pereira <naitwa.alexandre@gmail.com>
2021-02-28 02:43:07 -0300

    Almost done

--- src/components/NewsCard.js/NewsCard.js
+++ src/components/NewsCard.js/NewsCard.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 
+import useStyles from './styles.js';
 
-const NewsCard = () => {
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage, }, i}) => {
+    const classes = useStyles();
+
     return (
-        <div>
-            NewsCard
-        </div>
+        <Card className={classes.card}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia className={classes.media}  image={urlToImage || 'https://sitechecker.pro/wp-content/uploads/2017/12/404.png'}/>
+                <div className={classes.details}>
+                    <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+                </div>
+                <Typography className={classes.title} gutterBottom="h5">{title}</Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary">Lern More</Button>
+                <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
+            </CardActions>
+        </Card>
     )
 }
 
