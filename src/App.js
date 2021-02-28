82b20a4dd1ee3465778d98a0c76567372ecc3e91
Alexandre Pereira <naitwa.alexandre@gmail.com>
2021-02-28 04:57:36 -0300

    Scrolls

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
 
 
 const alanKey = 'b87f9069f1c55e172b8a11681e06c6802e956eca572e1d8b807a3e2338fdd0dc/stage';
@@ -9,6 +10,9 @@ const alanKey = 'b87f9069f1c55e172b8a11681e06c6802e956eca572e1d8b807a3e2338fdd0d
 const App = () => {
 
     const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const alanLogoSrc = 'https://voicebot.ai/wp-content/uploads/2019/10/alan.jpg';
 
     useEffect(() => {
         alanBtn({
@@ -16,6 +20,9 @@ const App = () => {
             onCommand: ({ command, articles }) => {
                 if(command === 'newHeadlines') {
                     setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if(command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
                 }
             }
         })
@@ -23,8 +30,10 @@ const App = () => {
 
     return (
         <div>
-            <h1>Alan AI News Aplication</h1>
-            <NewsCards articles={newsArticles} />
+            <div className={classes.logoContainer}>
+                <img src={alanLogoSrc} className={classes.alanLogo} alt="alanLogo"/>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
         </div>
     )
 }
